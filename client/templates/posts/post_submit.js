08d361523c869e2f3c25b8352d6c9bb2fe64ee3c
mrkbryn <mabryan.mb@gmail.com>
2016-01-05 13:10:01 -0600

    Enforce post URL uniqueness.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,6 +11,11 @@ Template.postSubmit.events({
 			// display the error to the user and abort
 			if (error)
 				return alert(error.reason);
+
+			// show this result but route anyway
+			if (result.postExists)
+				alert('This link has already been posted');
+
 			Router.go('postPage', {_id: result._id});
 		});
 	}
