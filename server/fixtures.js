91cf1a906ad9732fae026e34f690c99c9c754809
mrkbryn <mabryan.mb@gmail.com>
2016-01-05 17:56:22 -0600

    Denormalized the number of comments into the post

--- server/fixtures.js
+++ server/fixtures.js
@@ -16,7 +16,8 @@ if (Posts.find().count() === 0) {
 		userId: sacha._id,
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope/',
-		submitted: new Date(now - 7 * 3600 * 1000)
+		submitted: new Date(now - 7 * 3600 * 1000),
+		commentsCount: 2
 	});
 
 	Comments.insert({
@@ -40,7 +41,8 @@ if (Posts.find().count() === 0) {
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://meteor.com',
-		submitted: new Date(now - 10 * 3600 * 1000)
+		submitted: new Date(now - 10 * 3600 * 1000),
+		commentsCount: 0
 	});
 
 	Posts.insert({
@@ -48,6 +50,7 @@ if (Posts.find().count() === 0) {
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
-		submitted: new Date(now - 12 * 3600 * 1000)
+		submitted: new Date(now - 12 * 3600 * 1000),
+		commentsCount: 0
 	});
 }
